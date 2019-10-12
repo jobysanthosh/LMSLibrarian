d8c5c12712d9f6949ff660669ddb01f544b9a8bd
jobysanthosh <joby.santhosh@smoothstack.com>
2019-10-12 12:29:31 -0400

    Project Set Up

---
+++ LMSLibrarian/src/main/java/com/lms/LMSLibrarian/LmsLibrarianApplication.java
@@ -0,0 +1,13 @@
+package com.lms.LMSLibrarian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsLibrarianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsLibrarianApplication.class, args);
+	}
+
+}
