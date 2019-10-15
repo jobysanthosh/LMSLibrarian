e604730ff33a56ce0d22b408b7a8b4cea73b6a7b
jobysanthosh <joby.santhosh@smoothstack.com>
2019-10-15 13:30:40 -0400

    latest pojo

---
+++ LMSLibrarian/src/main/java/com/lms/LMSLibrarian/POJO/Book.java
@@ -0,0 +1,50 @@
+package com.lms.LMSLibrarian.POJO;
+
+public class Book {
+	private Integer bookId;
+	private String title;
+	private Author author;
+	private Publisher publisher;
+	
+	public Book() {}
+	
+	public Book(Integer bookId, String title, Author author, Publisher publisher) {
+		super();
+		this.bookId = bookId;
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+	}
+
+	public Integer getBookId() {
+		return bookId;
+	}
+	
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
+}
